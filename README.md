81ae9d4a1848f9875778210b7e7b8d5994e67186
sadamitsu yushi <uc.sadamitsu@gmail.com>
2018-10-26 20:06:31 +0900

    app 追加

--- README.md
+++ README.md
@@ -16,6 +16,17 @@ $ electron .
 
 WEB会議や、プレゼンテーションで使うと今までとは一味違ったコミュニケーションを楽しめますよ。
 
+### 環境構築がめんどくさいという方はこちら
+
+ファイルサイズは重くなりますが、パッケージ化しましたので、下記ZIPファイルをダウンロードして解凍しnicoG.exeファイルを実行すれば起動します。
+
+* windows_64bit[app/nicoG-win32-x64.zip]
+* windows_32bit[app/nicoG-win32-ia32.zip]
+* mac_64bit[app/nicoG-darwin-x64.zip]
+* linux_x86_64bit[app/nicoG-linux-x64.zip]
+
+アプリの終了の仕方が上手くいっておらず。残存ファイルが残ってしまったので終了後タスクマネージャーでnicoG.exeのタスクを終了させてください。
+
 ### 参考にしたサイトは
 
 [ニコニコメソッドプレゼンを全社会議に取り入れてみたら会議が面白くなった](http://tech.uzabase.com/entry/2015/06/01/143202)
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const elc_app = electron.app;
 const elc_BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
+var subWindow = null;
 elc_app.on('ready', function () {
     var size = electron.screen.getPrimaryDisplay().size;
     mainWindow = new elc_BrowserWindow({
@@ -26,4 +27,26 @@ elc_app.on('ready', function () {
     mainWindow.on('closed', function () {
       mainWindow = null;
       });
+
+
+      subWindow = new elc_BrowserWindow({
+        left: 0,
+        top: 0,
+        width: 400,
+        height: size.height,
+        frame: true,
+        show: true,
+        transparent: false,
+        resizable: true,
+        webPreferences: {nodeIntegration: false}
+      });
+//      subWindow.setIgnoreMouseEvents(true);
+//      subWindow.maximize();
+//      subWindow.setAlwaysOnTop(true);
+      subWindow.loadURL("https://nico-chat.herokuapp.com/controller");
+  //    subWindow.openDevTools();
+      subWindow.on('closed', function () {
+        subWindow = null;
+        });
+
 });
